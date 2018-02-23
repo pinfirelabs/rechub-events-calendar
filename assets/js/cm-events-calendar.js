497fb67db8e54a51b3937be8920926dcb47bb115
Justin Cherniak <jcherniak@pinfirelabs.com>
2018-02-23 15:44:50 -0800

    CM-2523 - Add some polyfills so this renders in old versions of IE, even if quite broken

--- assets/js/cm-events-calendar.js
+++ assets/js/cm-events-calendar.js
@@ -1,4 +1,4 @@
-(function($)
+var PcmCalendar = function($)
  {
 	if (typeof(clubmanager_timezone) == 'undefined')
 	{
@@ -282,4 +282,57 @@
 			$('#cm-events-calendar').fullCalendar('refetchEvents');
 		}
 	});
-})(jQuery);
+};
+
+// Old IE polyfills
+// From https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Object/keys
+if (!Object.keys) {
+  Object.keys = (function() {
+    'use strict';
+    var hasOwnProperty = Object.prototype.hasOwnProperty,
+        hasDontEnumBug = !({ toString: null }).propertyIsEnumerable('toString'),
+        dontEnums = [
+          'toString',
+          'toLocaleString',
+          'valueOf',
+          'hasOwnProperty',
+          'isPrototypeOf',
+          'propertyIsEnumerable',
+          'constructor'
+        ],
+        dontEnumsLength = dontEnums.length;
+
+    return function(obj) {
+      if (typeof obj !== 'function' && (typeof obj !== 'object' || obj === null)) {
+        throw new TypeError('Object.keys called on non-object');
+      }
+
+      var result = [], prop, i;
+
+      for (prop in obj) {
+        if (hasOwnProperty.call(obj, prop)) {
+          result.push(prop);
+        }
+      }
+
+      if (hasDontEnumBug) {
+        for (i = 0; i < dontEnumsLength; i++) {
+          if (hasOwnProperty.call(obj, dontEnums[i])) {
+            result.push(dontEnums[i]);
+          }
+        }
+      }
+      return result;
+    };
+  }());
+}
+
+if (!Array.isArray) {
+	Array.isArray = function(v) {
+		return v instanceof Array;
+	}
+}
+
+
+// Now that we have those covered, initialize the calendar now.
+PcmCalendar(jQuery);
